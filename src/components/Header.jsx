2fb54338da5c56daa7b519e2752bf3686a04b066
SdtElectronics <meiyang423@Gmail.com>
2022-10-09 16:32:06 -0500

    Implement add a place modal
    * add state to App.js
    * change the state upon form submission

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () =>{
     return(
         <header className="header">
             <h1>NightsOut</h1>
-            <button className="menu-button">Menu</button>
+            <button className="menu-button" style={{display: 'none'}}>Menu</button>
         </header>
     )
 }
--- src/components/addPlaceForm.jsx
+++ src/components/addPlaceForm.jsx
@@ -1,5 +1,15 @@
 import "./addPlaceForm.css";
 
+const cb = (e, data, setData) => {
+  e.preventDefault();
+  data[document.getElementById("placeName").value] = {
+		"numVisited": 0,
+		"datesVisited": []
+	};
+  setData(data);
+  console.log(data);
+};
+
 const AddPlaceForm = ({data, setData}) => {
     return (
         <form>
@@ -7,11 +17,9 @@ const AddPlaceForm = ({data, setData}) => {
                 <label htmlFor="placeName">Place</label>
                 <input className="form-control" type="text" id="placeName" placeholder="Your New Favorite Place"></input>
             </div>
-            <button type="submit" class="btn btn-outline-danger">Submit</button>
+            <button type="submit" className="btn btn-outline-danger" onClick={e => cb(e, data, setData)}>Submit</button>
         </form>
     )
-
-}
-
+};
 
 export default AddPlaceForm;
